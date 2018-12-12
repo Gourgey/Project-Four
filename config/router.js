425d14df9b86b135c784bd7ba4662ed889cccd0a
Gourgey <joshuagourgey@gmail.com>
2018-12-12 11:12:38 +0000

    added a backend purchases router

--- config/router.js
+++ config/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const paintings = require('../controllers/paintings');
 const auth = require('../controllers/auth');
 const user = require('../controllers/user');
+const purchases = require('../controllers/purchases');
 
 router.route('/paintings')
   .get(paintings.index)
@@ -11,6 +12,9 @@ router.route('/paintings/:id')
   .get(paintings.show)
   .put(paintings.update)
   .delete(paintings.delete);
+
+router.post('/checkout', purchases.create);
+router.get('/mypurchases', purchases.myPurchasesIndex);
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
